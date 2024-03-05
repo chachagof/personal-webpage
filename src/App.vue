bf2cce66ca2f10995044c2667a4ee778d7d949be
chachagof <chacha0932@gmail.com>
2024-03-05 17:17:28 +0800

    feat:create nav component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <script setup>
+import Nav from './components/Nav.vue'
 </script>
 
 <template>
+  <nav>
+    <Nav />
+  </nav>
 </template>
+
+<style scoped>
+nav {
+  background-color: #fff;
+  position: sticky;
+  top: 0;
+  padding: 0 85px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 80px;
+  box-shadow: 0 5px 80px rgba(0, 0, 0, 0.1);
+}
+
+@media screen and (max-width: 768px) {
+  nav {
+    justify-content: space-between;
+    padding: 0 42px;
+  }
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,119 @@
+<script setup>
+import { ref } from 'vue'
+
+const menuOpen = ref(false)
+const toggleMenu = () => {
+  menuOpen.value = !menuOpen.value
+}
+</script>
+
+<template>
+  <div class="logo" :class="{ open: menuOpen }">
+    <img src="../assets/C-logo.jpg" alt="logo">
+    <a href="https://youtube.com/@AsmrProg" target="_blank">Charlie</a>
+  </div>
+  <ul>
+    <li><a href="#main">Home</a></li>
+    <li><a href="#skills">Skills</a></li>
+    <li><a href="#portfolio">Portfolio</a></li>
+    <li><a href="#Experience">Experience</a></li>
+    <li><a href="#feedback">Feedback</a></li>
+  </ul>
+  <button>Contact Me</button>
+  <button id="menuButton" @click="toggleMenu">
+    <i class="bx bx-menu"></i>
+  </button>
+</template>
+
+<style scoped>
+.logo {
+  display: flex;
+  align-items: center;
+}
+
+.logo a {
+  margin-left: 8px;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.logo img {
+  width: 40px;
+  height: 40px;
+}
+
+.logo i {
+  font-size: 40px;
+}
+
+ul {
+  display: flex;
+  gap: 40px;
+}
+
+button {
+  color: #1c1e53;
+  background-color: transparent;
+  padding: 12px 16px;
+  border-radius: 5px;
+  border: 1px solid #1c1e53;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+button:hover {
+  background-color: #5e3bee;
+  border-color: #5e3bee;
+  color: #fff;
+}
+
+button#menuButton {
+  display: none;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  padding: 7px;
+}
+
+@media screen and (max-width: 992px) {
+  button {
+    display: none;
+  }
+
+  ul {
+    gap: 20px;
+  }
+
+  ul li a {
+    font-size: 15px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  ul {
+    display: none;
+  }
+
+  .open {
+    display: none;
+  }
+
+  .open+ul {
+    display: flex;
+  }
+
+  button#menuButton {
+    display: flex;
+  }
+}
+
+@media screen and (max-width: 576px) {
+  ul li a {
+    font-size: 13px;
+  }
+
+  .logo a {
+    font-size: 18px;
+  }
+}
+</style>
